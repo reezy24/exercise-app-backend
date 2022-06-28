ecccb3db1b2157d3f52746bb30754ee9caa99e92
Simon Curran <simon.curran@entaingroup.com.au>
2022-06-28 22:08:00 +1000

    updates default port to 5000 and installs cors module

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-app.use(express.json())
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
 
-app.get('/ping', (_, res) => {
-  res.send('pong')
-})
+app.get("/ping", (_, res) => {
+  res.send("pong");
+});
 
-app.post('/echo', (req, res) => {
-  res.send(req.body)
-})
+app.post("/echo", (req, res) => {
+  res.send(req.body);
+});
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
