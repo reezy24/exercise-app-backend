1e3eb22495910cb27773fe718f052e790bd8537c
reez <zreye24@gmail.com>
2022-07-03 09:36:59 +1000

    feat: connect to db

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 4000;
 
+const pgp = require('pg-promise')()
+const db = pgp({
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: process.env.POSTGRES_PORT || 5432,
+  database: process.env.POSTGRES_DB_NAME || 'exercise-app',
+  user: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+})
+
 app.use(express.json());
 app.use(cors());
 
@@ -15,6 +24,16 @@ app.get("/ping", (_, res) => {
 app.post("/echo", (req, res) => {
   res.send(req.body);
 });
+
+app.get('/users', (req, res) => {
+  db.any('select * from users')
+    .then((data) => {
+      res.send(data)
+    })
+    .catch((error) => {
+      res.send(error)
+    })
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
