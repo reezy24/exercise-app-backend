b1a172518651cdc421ea976dd119dc4b045697aa
reez <zreye24@gmail.com>
2022-07-03 10:28:37 +1000

    feat: connect to heroku postgres instance

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const cors = require("cors");
 const port = process.env.PORT || 4000;
 
 const pgp = require('pg-promise')()
-const db = pgp({
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: process.env.POSTGRES_PORT || 5432,
-  database: process.env.POSTGRES_DB_NAME || 'exercise-app',
-  user: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
-})
+const db = pgp(buildDBConnectionObj())
+
+function buildDBConnectionObj() {
+  let db_url = process.env.DATABASE_URL
+
+  if (!db_url) {
+    const db_host = process.env.POSTGRES_HOST || 'localhost'
+    const db_port = process.env.POSTGRES_PORT || 5432
+    const db_name = process.env.POSTGRES_DB_NAME || 'exercise-app'
+    const db_user = process.env.POSTGRES_USER || 'postgres'
+    const db_pass = process.env.POSTGRES_PASSWORD || 'postgres'
+
+    db_url = `postgres://${db_user}:${db_pass}@${db_host}:${db_port}/${db_name}`
+  }
+
+  return {
+    connectionString: db_url,
+    ssl: Boolean(process.env.POSTGRES_REQUIRE_SSL),
+  }
+}
 
 app.use(express.json());
 app.use(cors());
