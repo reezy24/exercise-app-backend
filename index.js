bcbb842655f9d9b71dca7bd5e3bc86d68f0bb20f
reez <zreye24@gmail.com>
2022-07-03 10:32:17 +1000

    feat: setup database connection
    * feat: create users table
    
    * feat: connect to db
    
    * feat: nodemon support
    
    * feat: connect to heroku postgres instance
    
    * chore: restructure db dir
    
    * chore: add note

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 4000;
 
+const pgp = require('pg-promise')()
+const db = pgp(buildDBConnectionObj())
+
+function buildDBConnectionObj() {
+  // Note: `DATABASE_URL` is the var name supplied by Heroku - do not change.
+  let db_url = process.env.DATABASE_URL
+
+  if (!db_url) {
+    const db_host = process.env.POSTGRES_HOST || 'localhost'
+    const db_port = process.env.POSTGRES_PORT || 5432
+    const db_name = process.env.POSTGRES_DB_NAME || 'exercise-app'
+    const db_user = process.env.POSTGRES_USER || 'postgres'
+    const db_pass = process.env.POSTGRES_PASSWORD || 'postgres'
+
+    db_url = `postgres://${db_user}:${db_pass}@${db_host}:${db_port}/${db_name}`
+  }
+
+  return {
+    connectionString: db_url,
+    ssl: Boolean(process.env.POSTGRES_REQUIRE_SSL),
+  }
+}
+
 app.use(express.json());
 app.use(cors());
 
@@ -15,6 +38,16 @@ app.get("/ping", (_, res) => {
 app.post("/echo", (req, res) => {
   res.send(req.body);
 });
+
+app.get('/users', (req, res) => {
+  db.any('select * from users')
+    .then((data) => {
+      res.send(data)
+    })
+    .catch((error) => {
+      res.send(error)
+    })
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
