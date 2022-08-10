e6e882744fb5bc33c2f5415191a937cb0865ca53
reez <zreye24@gmail.com>
2022-08-10 19:12:29 +1000

    chore: users router

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const passport = require('passport')
-
 const express = require("express");
 const session = require("express-session")
+const cors = require("cors");
+const port = process.env.PORT || 4000;
+
+// Use middlewares.
 const app = express();
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -13,36 +16,16 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
-
-const cors = require("cors");
-const port = process.env.PORT || 4000;
-
-const db = require("./database/connect")
-
 app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:3000'],
   credentials: true,
 }));
 
-const isLoggedIn = require('./middleware/isLoggedIn');
-
 // Sub routes.
 app.use('/', require("./routes/example"))
 app.use('/auth', require('./routes/auth'))
-
-app.get('/users', async (_, res, next) => {
-  try {
-    const dbres = await db.query('select * from users')
-    res.send(dbres.rows)
-  } catch (err) {
-    next(err)
-  }
-})
-
-app.get('/current-user', isLoggedIn, (req, res) => {
-  res.json(req.user)
-})
+app.use('/users', require('./routes/users'))
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
