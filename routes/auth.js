f9b061f890e891b68773757b5573b89355e0f0b4
reez <zreye24@gmail.com>
2022-08-14 14:18:31 +1000

    fix logout

--- routes/auth.js
+++ routes/auth.js
@@ -22,9 +22,22 @@ authRouter.get('/google/callback',
 
 // Log out the user.
 authRouter.get('/logout', (req, res) => {
-  req.logout()
-  req.session.destroy()
-  return res.send('Goodbye!')
+  return req.logout((err) => {
+    if (err) {
+      return next(err)
+    }
+    req.session.destroy()
+    res.redirect('/auth/logged-out')
+  })
+})
+
+authRouter.get('/logged-out', (req, res) => {
+  return res.send(`
+    <div>
+      <p>You have logged out.</p>
+      <a href="${process.env.FRONTEND_ORIGIN}">Back to home</a>
+    </div>
+  `)
 })
 
 module.exports = authRouter
